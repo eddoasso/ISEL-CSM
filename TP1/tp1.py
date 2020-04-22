a51dbcfa58b2fb893aa82634ac3b50486412fe6a
Pedro Henriques <35275083+eddoasso@users.noreply.github.com>
2020-04-22 16:07:45 +0100

    novos ficheiros

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -2,29 +2,25 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def ex1():
     x_img = cv2.imread("lenac.tif")
-    cv2.imshow('OriginalImg', x_img)
     print(x_img.dtype)
     print(x_img.shape)
 
     cv2.imwrite('file1.jpg', x_img, (cv2.IMWRITE_JPEG_QUALITY, 80))
     cv2.imwrite('file2.jpg', x_img, (cv2.IMWRITE_JPEG_QUALITY, 10))
 
-
-    y_img = cv2.imread("file1.jpg")
-    z_img = cv2.imread("file2.jpg")
+    img_80 = cv2.imread("file1.jpg")
+    img_10 = cv2.imread("file2.jpg")
 
     cv2.imshow('Original Image', x_img)
-    cv2.imshow('80% Image', y_img)
-    cv2.imshow('10% Image', z_img)
+    cv2.imshow('80 img', img_80)
+    cv2.imshow('10 img', img_10)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    x_img_g = cv2.cvtColor(x_img , cv2.COLOR_BGR2GRAY)
-    cv2.imshow (' Gray Image ' , x_img_g )
-    cv2.imwrite(' file3.bmp' , x_img_g )
 
 if __name__ == '__main__':
     ex1()
